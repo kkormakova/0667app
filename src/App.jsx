787208fa5836f2ace440d960ce585b07c8c9b7a3
kkormakova <109741103+kkormakova@users.noreply.github.com>
2022-08-26 11:54:49 +0300

    ain

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+const Profile = () => {
+  return (
+    <h2>Это страница с профилем</h2>
+  );
+}
+const Messages = () => {
+  return <h2>Это страница с сообщениями</h2>;
+}
+const Settings = () => {
+  return <h2>Это страница с настройками</h2>;
+}
+const Friends = () => {
+  return <h2>Это страница с друзьями</h2>;
+}
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+      <div className="col-3">
+        <div className="nav flex-column nav-pills" aria-orientation="vertical">
+          <NavLink className="nav-link" to="profile">Профиль</NavLink>
+          <NavLink className="nav-link" to="messages">Сообщения</NavLink>
+          <NavLink className="nav-link" to="settings">Настройки</NavLink>
+          <NavLink className="nav-link" to="friends">Друзья</NavLink>
+        </div>
+      </div>
+        <div className="col-9">
+          <Routes>
+            <Route path='/profile' element={<Profile/>}></Route>
+            <Route path='/messages' element={<Messages/>}></Route>
+            <Route path='/settings' element={<Settings/>}></Route>
+            <Route path='/friends' element={<Friends/>}></Route>
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
